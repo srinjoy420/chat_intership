7c95afb5fe12b9b5caf60277855f072fb1a0142d
srjonjoy <ghoshsrinjoy2@gmail.com>
2025-09-26 23:29:49 +0530

    complete

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,16 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv"
+import fs from "fs";
 dotenv.config()
 
 export const db = await mysql.createConnection({
-    host: process.env.HOSt,
-    user: process.env.USER,
+    host: process.env.HOSt || "localhost",
+    user: process.env.USER || "root",
     password: process.env.MY_SQL_PASSWORD,
-    database: "chatapp"
+    database: process.env.DB_NAME||"chatapp",
+    ssl: {
+    ca: fs.readFileSync(process.env.DB_CA)
+  }
 });
 
 console.log("mysql connect succesfully"); 
